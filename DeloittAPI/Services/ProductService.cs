069228e8e58a0af24131c15543a1bae957f48afb
Armando Antonio <armando.antoniodelangel@hotmail.com>
2024-06-17 16:46:50 -0600

    Change methods to async

--- DeloittAPI/Services/ProductService.cs
+++ DeloittAPI/Services/ProductService.cs
@@ -10,46 +10,50 @@ namespace DeloittAPI.Services
 
         public ProductService()
         {
-            // Initialize Product Dictionary
-            _products = new Dictionary<int, Product>
-        {
-            { 1, new Product { Id = 1, Quantity = 10, Capacity = 100 } },
-            { 2, new Product { Id = 2, Quantity = 20, Capacity = 200 } },
-        };
+            _products = InitializeProducts();
         }
 
-        public Task<ServiceResponse> GetAllProductsAsync()
+        private Dictionary<int, Product> InitializeProducts()
+        {
+            return new Dictionary<int, Product>
+            {
+                { 1, new Product { Id = 1, Quantity = 10, Capacity = 100 } },
+                { 2, new Product { Id = 2, Quantity = 20, Capacity = 200 } },
+            };
+        }
+
+        public async Task<ServiceResponse> GetAllProductsAsync()
         {
             try
             {
                 // Return all products
-                return Task.FromResult(ServiceResponse.CreateSuccess(_products.Values.AsEnumerable()));
+                return await Task.FromResult(ServiceResponse.CreateSuccess(_products.Values.AsEnumerable()));
             }
             catch (Exception ex)
             {
-                return Task.FromResult(ServiceResponse.CreateFailure(ex.Message, ServiceResponseStatus.InternalServerError));
+                return await Task.FromResult(ServiceResponse.CreateFailure(ex.Message, ServiceResponseStatus.InternalServerError));
             }
         }
 
-        public Task<ServiceResponse> SetProductCapacityAsync(int productId, int capacity)
+        public async Task<ServiceResponse> SetProductCapacityAsync(int productId, int capacity)
         {
             try
             {
                 if (!_products.ContainsKey(productId))
                 {
                     _products.Add(productId, new Product { Id = productId, Quantity = 0, Capacity = capacity });
-                    return Task.FromResult(ServiceResponse.CreateSuccess("Product added successfully."));
+                    return await Task.FromResult(ServiceResponse.CreateSuccess("Product added successfully."));
                 }
-                return Task.FromResult(ServiceResponse.CreateFailure("Product with given ID already exists.", ServiceResponseStatus.BadRequest));
+                return await Task.FromResult(ServiceResponse.CreateFailure("Product with given ID already exists.", ServiceResponseStatus.BadRequest));
             }
             catch (Exception ex)
             {
-                return Task.FromResult(ServiceResponse.CreateFailure(ex.Message, ServiceResponseStatus.InternalServerError));
+                return await Task.FromResult(ServiceResponse.CreateFailure(ex.Message, ServiceResponseStatus.InternalServerError));
             }
             
         }
 
-        public Task<ServiceResponse> ReceiveProductAsync(int productId, int quantity)
+        public async Task<ServiceResponse> ReceiveProductAsync(int productId, int quantity)
         {
             try
             {
@@ -58,20 +62,20 @@ namespace DeloittAPI.Services
                     if (product.Quantity + quantity <= product.Capacity)
                     {
                         product.Quantity += quantity;
-                        return Task.FromResult(ServiceResponse.CreateSuccess("Product received successfully."));
+                        return await Task.FromResult(ServiceResponse.CreateSuccess("Product received successfully."));
                     }
-                    return Task.FromResult(ServiceResponse.CreateFailure("Product quantity exceeds capacity.", ServiceResponseStatus.BadRequest));
+                    return await Task.FromResult(ServiceResponse.CreateFailure("Product quantity exceeds capacity.", ServiceResponseStatus.BadRequest));
                 }
-                return Task.FromResult(ServiceResponse.CreateFailure("Product not found.", ServiceResponseStatus.NotFound));
+                return await Task.FromResult(ServiceResponse.CreateFailure("Product not found.", ServiceResponseStatus.NotFound));
             }
             catch (Exception ex)
             {
-                return Task.FromResult(ServiceResponse.CreateFailure(ex.Message, ServiceResponseStatus.InternalServerError));
+                return await Task.FromResult(ServiceResponse.CreateFailure(ex.Message, ServiceResponseStatus.InternalServerError));
             }
           
         }
 
-        public Task<ServiceResponse> DispatchProductAsync(int productId, int quantity)
+        public async Task<ServiceResponse> DispatchProductAsync(int productId, int quantity)
         {
 
             try
@@ -81,16 +85,16 @@ namespace DeloittAPI.Services
                     if (quantity <= product.Quantity)
                     {
                         product.Quantity -= quantity;
-                        return Task.FromResult(ServiceResponse.CreateSuccess("Product received successfully."));
+                        return await Task.FromResult(ServiceResponse.CreateSuccess("Product received successfully."));
                     }
-                    return Task.FromResult(ServiceResponse.CreateFailure("Product quantity dispatch more than available.", ServiceResponseStatus.BadRequest));
+                    return await Task.FromResult(ServiceResponse.CreateFailure("Product quantity dispatch more than available.", ServiceResponseStatus.BadRequest));
 
                 }
-                return Task.FromResult(ServiceResponse.CreateFailure("Product not found.", ServiceResponseStatus.NotFound));
+                return await Task.FromResult(ServiceResponse.CreateFailure("Product not found.", ServiceResponseStatus.NotFound));
             }
             catch (Exception ex)
             {
-                return Task.FromResult(ServiceResponse.CreateFailure(ex.Message, ServiceResponseStatus.InternalServerError));
+                return await Task.FromResult(ServiceResponse.CreateFailure(ex.Message, ServiceResponseStatus.InternalServerError));
             }
          
         }
